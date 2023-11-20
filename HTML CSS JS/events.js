184f32124f4bf3b01d3b9c1b1eac8378694941f6
savger1024 <90380477+savger1024@users.noreply.github.com>
2023-11-20 22:12:18 +0100

    JS if
    Button id alapján eldönti, mit írjon ki alert box-ban

--- HTML CSS JS/events.js
+++ HTML CSS JS/events.js
@@ -1,3 +1,9 @@
-function Registration() {
-    alert("Thank you for your feedback!");
+function Registration(ids) {
+  if (ids === 'sprint') {
+    alert('Thank you for your feedback! Sprint starts at 15:00');
+  } else if (ids === 'marathon') {
+    alert(
+      "Thank you for your feedback! Marathon starts at 16:00. Don't forget to bring water!"
+    );
+  }
 }
