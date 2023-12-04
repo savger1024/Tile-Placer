451c89ad0d0e3059ce060a9ec78e87a5f9270715
savger1024 <savger1024@gmail.com>
2023-12-04 15:57:50 +0100

    Futó mozgás (bástya mozgás) id-k alapján

--- Tile Placer/sandbox/event.js
+++ Tile Placer/sandbox/event.js
@@ -7,8 +7,24 @@ function newTile(ids) {
     const divTag = document.createElement("div");
 
 
-
-    if (col < 5) {
+    if (row == 1 && col == 0) {
+        document.getElementById("row"+row).appendChild(divTag);
+        newCol = col  + 1;
+        //new tile properties
+        
+        divTag.id ="row"+row+"col"+newCol;
+        divTag.className = "tile active-tile";
+        divTag.onclick = function() {
+        colorChange(this);
+    };
+        if(ids === "red") divTag.style.backgroundColor = "rgb(255, 0, 0)";
+        else if(ids === "green") divTag.style.backgroundColor = "rgb(0, 128, 0)";
+        else if(ids === "blue") divTag.style.backgroundColor = "rgb(0, 0, 255)";
+        else if(ids === "black") divTag.style.backgroundColor = "rgb(0, 0, 0)";
+        else if(ids === "white") divTag.style.backgroundColor = "rgb(255, 228, 196)";           
+        colTag.innerHTML = newCol.toString();
+    }
+    else if (col < 5) {
         document.getElementById("row"+row).appendChild(divTag);
         newCol = col  + 1;
         //new tile properties
@@ -52,11 +68,81 @@ function newTile(ids) {
 
 function colorChange(tile) {
     var currentColor = window.getComputedStyle(tile).backgroundColor;
-    console.log("Current color:", tile.style.backgroundColor);
     /*red*/if (currentColor === "rgb(255, 0, 0)") tile.style.backgroundColor = "green";
     /*green*/else if (currentColor === "rgb(0, 128, 0)") tile.style.backgroundColor = "blue";
     /*blue*/else if (currentColor === "rgb(0, 0, 255)") tile.style.backgroundColor = "black";
     /*black*/else if (currentColor === "rgb(0, 0, 0)") tile.style.backgroundColor = "bisque";
     /*white*/else if (currentColor === "rgb(255, 228, 196)") tile.style.backgroundColor = "red";
-    console.log("Current color:", tile.style.backgroundColor);
+
+    
+    activeTile = document.getElementsByClassName("active-tile");
+
+    if (activeTile.length > 0) {
+    var firstElement = activeTile[0];
+    var activeIdString = firstElement.id;
+    }
+    else  console.log("No elements with the specified class found");
+    
+    counter = 0;
+    activeRowString = "";
+    activeColString = "";
+    
+    for (var i = 0; i < activeIdString.length; i++) {
+        var currentCharacter = activeIdString[i];
+        if (counter == 0 && !isNaN(currentCharacter)) {
+            activeRowString += currentCharacter; counter += 1;
+        }
+        else if (counter == 1) {
+            if (!isNaN(currentCharacter)) activeRowString += currentCharacter; else counter += 1;
+        }
+        else if (counter == 2 && !isNaN(currentCharacter)) {
+            activeColString += currentCharacter; counter += 1;
+        }  
+        else if (counter == 3) {
+            if (!isNaN(currentCharacter)) activeColString += currentCharacter; else counter += 1;
+        }
+        
+    }
+
+    activeCol = parseInt(activeColString);
+    activeRow = parseInt(activeRowString);
+
+
+    counter = 0;
+    clickedRowString = "";
+    clickedColString = "";
+
+    for (var i = 0; i < tile.id.length; i++) {
+        var currentCharacter = tile.id[i];
+        if (counter == 0 && !isNaN(currentCharacter)) {
+            clickedRowString += currentCharacter; counter += 1;
+        }
+        else if (counter == 1) {
+            if (!isNaN(currentCharacter)) clickedRowString += currentCharacter; else counter += 1;
+        }
+        else if (counter == 2 && !isNaN(currentCharacter)) {
+            clickedColString += currentCharacter; counter += 1;
+        }  
+        else if (counter == 3) {
+            if (!isNaN(currentCharacter)) clickedColString += currentCharacter; else counter += 1;
+        }
+    }
+    clickedCol = parseInt(clickedColString);
+    clickedRow = parseInt(clickedRowString);
+    //activeCol = parseInt(activeColString);
+    //activeRow = parseInt(activeRowString);
+
+
+    //bástya mozgás
+    /*if (activeCol==clickedCol || activeRow==clickedRow) {
+        tile.classList.toggle("active-tile");
+        firstElement.classList.toggle("active-tile");
+    }*/
+    //futó mozgás
+    if (((activeCol-clickedCol)/(activeRow-clickedRow)==1) || ((activeCol-clickedCol)/(activeRow-clickedRow)==-1)) {
+        tile.classList.toggle("active-tile");
+        firstElement.classList.toggle("active-tile");
+    }
+
+
 }
