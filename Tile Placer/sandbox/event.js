12379f0708f76e5a03ffa177475cc35f5f10b22a
savger1024 <savger1024@gmail.com>
2023-12-06 16:57:01 +0100

    active-tile + WhiteFilter együtt működik; igazítás

--- Tile Placer/sandbox/event.js
+++ Tile Placer/sandbox/event.js
@@ -5,31 +5,27 @@ function newTile(ids) {
     col = parseInt(colTag.innerHTML);
     row = parseInt(rowTag.innerHTML);
     const divTag = document.createElement("div");
-
-
     if (row == 1 && col == 0) {
         document.getElementById("row"+row).appendChild(divTag);
         newCol = col  + 1;
         //new tile properties
-        
         divTag.id ="row"+row+"col"+newCol;
         divTag.className = "tile active-tile light-tile";
         divTag.onclick = function() {
             colorChange(this);
+            unitMove(this);
         };
         if(ids === "red") divTag.style.backgroundColor = "rgb(255, 0, 0)"; //
         else if(ids === "green") divTag.style.backgroundColor = "rgb(0, 128, 0)"; //
         else if(ids === "blue") divTag.style.backgroundColor = "rgb(0, 0, 255)"; //
         else if(ids === "black") divTag.style.backgroundColor = "rgb(0, 0, 0)"; //
         else if(ids === "white") divTag.style.backgroundColor = "rgb(255, 228, 196)"; //
-        
         colTag.innerHTML = newCol.toString();
     }
     else if (col < 4) {
         document.getElementById("row"+row).appendChild(divTag);
         newCol = col  + 1;
         //new tile properties
-        
         divTag.id ="row"+row+"col"+newCol;
         if (col % 2 == 0 && row % 2 == 1) divTag.className = "tile light-tile";
         else if (col % 2 == 1 && row % 2 == 1) divTag.className = "tile dark-tile";
@@ -38,6 +34,7 @@ function newTile(ids) {
         
         divTag.onclick = function() {
             colorChange(this);
+            unitMove(this);
         }
         if(ids === "red") divTag.style.backgroundColor = "rgb(255, 0, 0)";
         else if(ids === "green") divTag.style.backgroundColor = "rgb(0, 128, 0)";
@@ -56,9 +53,10 @@ function newTile(ids) {
         divTag.id ="row"+newRow+"col1";
         if (row % 2 == 1) divTag.className = "tile dark-tile";
         else divTag.className = "tile light-tile";
-        
+
         divTag.onclick = function() {
         colorChange(this);
+        unitMove(this);
         };
         if(ids === "red") divTag.style.backgroundColor = "rgb(255, 0, 0)";
         else if(ids === "green") divTag.style.backgroundColor = "rgb(0, 128, 0)";
@@ -69,31 +67,19 @@ function newTile(ids) {
         colTag.innerHTML = "1";
         rowTag.innerHTML = newRow.toString();
     }
-    
-
 }
-
-function colorChange(tile) {
-    var currentColor = window.getComputedStyle(tile).backgroundColor;
-    /*red*/if (currentColor === "rgb(255, 0, 0)") tile.style.backgroundColor = "green";
-    /*green*/else if (currentColor === "rgb(0, 128, 0)") tile.style.backgroundColor = "blue";
-    /*blue*/else if (currentColor === "rgb(0, 0, 255)") tile.style.backgroundColor = "black";
-    /*black*/else if (currentColor === "rgb(0, 0, 0)") tile.style.backgroundColor = "bisque";
-    /*white*/else if (currentColor === "rgb(255, 228, 196)") tile.style.backgroundColor = "red";
-
-    
+function unitMove(unit) {
+    console.log("Hello from unitMove");
     activeTile = document.getElementsByClassName("active-tile");
-
     if (activeTile.length > 0) {
     var firstElement = activeTile[0];
     var activeIdString = firstElement.id;
     }
     else  console.log("No elements with the specified class found");
-    
+    //Identifying origin coordinates
     counter = 0;
     activeRowString = "";
     activeColString = "";
-    
     for (var i = 0; i < activeIdString.length; i++) {
         var currentCharacter = activeIdString[i];
         if (counter == 0 && !isNaN(currentCharacter)) {
@@ -108,19 +94,15 @@ function colorChange(tile) {
         else if (counter == 3) {
             if (!isNaN(currentCharacter)) activeColString += currentCharacter; else counter += 1;
         }
-        
     }
-
     activeCol = parseInt(activeColString);
     activeRow = parseInt(activeRowString);
-
-
+    //identifying destination coordinates
     counter = 0;
     clickedRowString = "";
     clickedColString = "";
-
-    for (var i = 0; i < tile.id.length; i++) {
-        var currentCharacter = tile.id[i];
+    for (var i = 0; i < unit.id.length; i++) {
+        var currentCharacter = unit.id[i];
         if (counter == 0 && !isNaN(currentCharacter)) {
             clickedRowString += currentCharacter; counter += 1;
         }
@@ -134,23 +116,43 @@ function colorChange(tile) {
             if (!isNaN(currentCharacter)) clickedColString += currentCharacter; else counter += 1;
         }
     }
+    //activeCol = parseInt(activeColString);
+    //activeRow = parseInt(activeRowString);}
     clickedCol = parseInt(clickedColString);
     clickedRow = parseInt(clickedRowString);
-    //activeCol = parseInt(activeColString);
-    //activeRow = parseInt(activeRowString);
 
-
-    //bástya mozgás
-    if (activeCol==clickedCol || activeRow==clickedRow) {
-        tile.classList.toggle("active-tile");
+    //move validation, execution
+    if (activeCol==clickedCol || activeRow==clickedRow) { //bástya mozgás
+        unit.classList.toggle("active-tile");
         firstElement.classList.toggle("active-tile");
+        tileRefresh(unit);
+        tileRefresh(document.getElementById(activeIdString));
     }
-    //futó mozgás
-    //Math.abs(activeCol-clickedCol) < 2 | Math.abs(activeCol-clickedCol) % 5 < 2
-    /*if (((activeCol-clickedCol)/(activeRow-clickedRow)==1) || ((activeCol-clickedCol)/(activeRow-clickedRow)==-1)) {
+    /*if (Math.abs((activeCol-clickedCol)/(activeRow-clickedRow)==1)) { //futó mozgás (nem működik?)
         tile.classList.toggle("active-tile");
         firstElement.classList.toggle("active-tile");
     }*/
+    //Math.abs(activeCol-clickedCol) < 2 | Math.abs(activeCol-clickedCol) % 5 < 2
+}
 
+function tileRefresh(ids) { //correct display of tiles
+    bgImg = "";
+    let tileElement = document.getElementById(ids.id);
+    if (tileElement.classList.contains("light-tile"))
+        bgImg += "url('https://i.imgur.com/B3Pj5xc.png')";
+    if (tileElement.classList.contains("dark-tile"))
+        bgImg += "url('https://i.imgur.com/7gkqAFk.png')";    
+    if (tileElement.classList.contains("active-tile"))
+        bgImg += ", url('https://i.imgur.com/ZSf7hoY.png') "; //,
+    if (bgImg == "") bgImg += "none";
+    tileElement.style.backgroundImage = bgImg;
+};
 
+function colorChange(tile) {
+    var currentColor = window.getComputedStyle(tile).backgroundColor;
+    /*red*/if (currentColor === "rgb(255, 0, 0)") tile.style.backgroundColor = "green";
+    /*green*/else if (currentColor === "rgb(0, 128, 0)") tile.style.backgroundColor = "blue";
+    /*blue*/else if (currentColor === "rgb(0, 0, 255)") tile.style.backgroundColor = "black";
+    /*black*/else if (currentColor === "rgb(0, 0, 0)") tile.style.backgroundColor = "bisque";
+    /*white*/else if (currentColor === "rgb(255, 228, 196)") tile.style.backgroundColor = "red";
 }
